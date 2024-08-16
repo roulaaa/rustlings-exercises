3742b58007e2880e5dc697538c7b002f1b07fd6e
roulaaa <roula.ghaleb@lau.edu>
2024-08-16 23:54:55 +0300

    solved intro

--- exercises/00_intro/intro2.rs
+++ exercises/00_intro/intro2.rs
@@ -1,4 +1,9 @@
 fn main() {
     // TODO: Fix the code to print "Hello world!".
-    printline!("Hello world!");
+    println!("Hello world!");
+    println!("{number:0>width$}", number=1, width=5);
+    println!("{number:0>5}", number=1); // 00001
+
+
+
 }
