c748c69b00508e63d2103034802428bc3bb9830c
roulaaa <roula.ghaleb@lau.edu>
2024-08-17 02:09:13 +0300

    solved 01_variables
    AA

--- exercises/01_variables/variables2.rs
+++ exercises/01_variables/variables2.rs
@@ -1,10 +1,11 @@
 fn main() {
     // TODO: Change the line below to fix the compiler error.
-    let x;
-
+    let x = 10;
+    
     if x == 10 {
         println!("x is ten!");
     } else {
         println!("x is not ten!");
     }
+    
 }
